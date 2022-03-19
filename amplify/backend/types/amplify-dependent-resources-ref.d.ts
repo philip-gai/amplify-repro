23bbbc788d369f694d4addf6ddb1bb7afb5fb0e7
Philip Gai <PhilipMGai@gmail.com>
2022-03-19 04:36:11 +0000

    Init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifyrepro": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
